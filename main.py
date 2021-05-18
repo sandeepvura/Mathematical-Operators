e0bcd80416da67c9131e5f33505d29933d5ebb16
sandeepvura <>
2021-05-18 17:24:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+print(int(3 * 3 + 3 / 3 - 3))
+# b = print(int(a))
+
+**Mathematical Operators**
+
+```
+###Adding two numbers
+
+2 + 2
+
+### Multiplication 
+
+3 * 2
+
+### Subtraction 
+
+4 - 2
+
+### Division
+
+6 / 3
+
+### Exponential 
+
+When you want to raise a number
+
+2 ** 2 = 4
+2 ** 3 = 8
+
+```
+
+***Note:*** Order of Mathematical operators to execute is like "PEMDAS"
+P -  Parentheses - ()
+E - Exponents - **
+M - Multiplication - *
+D - Division - /
+A - Addition - +
+S - Subtraction - 
+
+*Example* : Rule of PEMDAS
+
+```
+print( 3 * 3 + 3 / 3 - 3)  #Find the output. 
+
+```
